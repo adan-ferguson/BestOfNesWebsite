5fd0c9ded23cd7ed47afc38a9024b37e34f01be1
Adan Ferguson <adanferguson@gmail.com>
2018-05-10 13:31:01 -0600

    Watching

--- app/gulp/begin.js
+++ app/gulp/begin.js
@@ -2,6 +2,9 @@ const cleanup = require('./cleanup.js')
 const compile = require('./compile.js')
 const inject = require('./inject.js')
 const startserver = require('./startserver.js')
+const log = require('fancy-log')
+
+const watch = require('gulp-watch')
 
 module.exports = async (gulp, options) => {
 
@@ -13,4 +16,31 @@ module.exports = async (gulp, options) => {
   if(options.startServer){
     startserver()
   }
+
+  watch(['web/js/**/*'], {events: ['change']}, () => {
+    compile.writeJS(gulp, options.mode)
+    log('Recompiled JS')
+  })
+
+  watch(['web/js/**/*'], {events: ['add']}, async () => {
+    await compile.writeJS(gulp, options.mode)
+    inject(gulp, options.mode)
+    log('Recompiled and injected JS')
+  })
+
+  watch(['web/styles/**/*'], {events: ['change']}, () => {
+    compile.writeStyles(gulp, options.mode)
+    log('Recompiled CSS')
+  })
+
+  watch(['web/styles/**/*'], {events: ['add']}, async () => {
+    await compile.writeStyles(gulp, options.mode)
+    inject(gulp, options.mode)
+    log('Recompiled and injected CSS')
+  })
+
+  watch(['web/views/**/*'], {events: ['change']}, () => {
+    inject(gulp, options.mode)
+    log('Injected')
+  })
 }
