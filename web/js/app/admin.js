dd1925966cd6d0e8006dda296ca6532e96b9fef9
Adan Ferguson <adanferguson@gmail.com>
2018-07-14 12:58:08 -0600

    Tabbing

--- web/js/app/admin.js
+++ web/js/app/admin.js
@@ -2,13 +2,59 @@
 
   const Admin = {}
 
+  let form
+
   Admin.initRaceEditPage = function(){
 
-    let form = document.querySelector('form#edit-race')
+    form = document.querySelector('form#edit-race')
+
+    tabs()
+    pikaday()
+    submit()
+  }
+
+  function tabs(){
+
+    let tabButtons = form.querySelectorAll('.nav-link')
+    let sections = form.querySelectorAll('section')
+    let currentIndex = 0
+
+    tabButtons.forEach(btn => {
+      btn.addEventListener('click', e => {
+
+        let newIndex = getElementIndex(e.target.parentElement)
+
+        if(newIndex === currentIndex){
+          return
+        }
+
+        tabButtons[currentIndex].classList.remove('active')
+        sections[currentIndex].classList.remove('active')
+        tabButtons[newIndex].classList.add('active')
+        sections[newIndex].classList.add('active')
+
+        currentIndex = newIndex
+      })
+    })
+  }
+
+  function getElementIndex(element){
+
+    let i = -1
+    while(element){
+      i++
+      element = element.previousElementSibling
+    }
+    return i
+  }
+
+  function pikaday(){
     new window.Pikaday({
       field: form.querySelector('.date-input')
     })
+  }
 
+  function submit(){
     form.addEventListener('submit', e => {
       console.log('submitting')
       e.preventDefault()
