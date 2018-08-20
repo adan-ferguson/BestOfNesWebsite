355a1938b731ee0ddc7a26de5c1a6a078346456f
Adan Ferguson <adanferguson@gmail.com>
2018-08-19 22:28:04 -0600

    Marathon/race pages

--- app/routes/public.js
+++ app/routes/public.js
@@ -2,7 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('homepage', {title: 'Hey', message: 'Route message'})
+  res.render('homepage', {title: 'Homepage'})
+})
+
+router.get('/marathons', (req, res) => {
+  res.render('marathons', {title: 'Marathons'})
+})
+
+router.get('/races', (req, res) => {
+  res.render('races', {title: 'Races'})
 })
 
 router.get('/twitchredirect', (req, res) => {
