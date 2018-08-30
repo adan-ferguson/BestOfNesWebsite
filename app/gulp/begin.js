d8fdabf3fac154748a04333ed21ede32dc196735
Adan Ferguson <adanferguson@gmail.com>
2018-08-29 22:38:27 -0600

    re-inject on change
    because the rev() changes the filenames

--- app/gulp/begin.js
+++ app/gulp/begin.js
@@ -12,23 +12,13 @@ module.exports = async (gulp, options) => {
   await compile.writeStyles(gulp, options.mode)
   inject(gulp, options.mode)
 
-  watch(['web/js/**/*'], {events: ['change']}, () => {
-    compile.writeJS(gulp, options.mode)
-    log('Recompiled JS')
-  })
-
-  watch(['web/js/**/*'], {events: ['add']}, async () => {
+  watch(['web/js/**/*'], async () => {
     await compile.writeJS(gulp, options.mode)
     inject(gulp, options.mode)
     log('Recompiled and injected JS')
   })
 
-  watch(['web/styles/**/*'], {events: ['change']}, () => {
-    compile.writeStyles(gulp, options.mode)
-    log('Recompiled CSS')
-  })
-
-  watch(['web/styles/**/*'], {events: ['add']}, async () => {
+  watch(['web/styles/**/*'], async () => {
     await compile.writeStyles(gulp, options.mode)
     inject(gulp, options.mode)
     log('Recompiled and injected CSS')
